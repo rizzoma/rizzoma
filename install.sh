d1d92b0d9cb44e7cdf0b3fb8bfbce1983b319188
Robin Tibor Schirrmeister <robintibor@gmail.com>
2014-07-14 23:41:01 +0200

    finished first version of shell installation script

--- install.sh
+++ install.sh
@@ -10,8 +10,12 @@ cp src/server/settings_local.coffee.template src/server/settings_local.coffee
 
 # Configure Sphinxsearch
 sudo sed -i "s/START=no/START=yes/" /etc/default/sphinxsearch
-./bin/edit-sudoers-for-sphinxsearch.sh
+./bin/edit_sudoers_for_sphinxsearch.sh
+# copy sphinx conf and insert actual rizzoma directory
+DIRECTORY=`pwd`
 sudo cp ./etc/sphinxsearch/sphinx.conf /etc/sphinxsearch/sphinx.conf
+sudo sed -i "s#/path_to_project#${DIRECTORY}#" /etc/sphinxsearch/sphinx.conf
+# stop daily rotating
 sudo cp ./etc/cron.d/sphinxsearch /etc/cron.d/sphinxsearch 
 
 # Build rizzoma and init sphinxsearch
